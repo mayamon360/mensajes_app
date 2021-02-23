c0013ca3e2d902be2bead4b094d68ffbb151b04f
DESKTOP-QDBK0G5\Alma <ti0911291m@gmail.com>
2021-02-23 11:49:06 -0600

    Cuarto commit

--- src/main/java/com/platzi/mensajes_app/Inicio.java
+++ src/main/java/com/platzi/mensajes_app/Inicio.java
@@ -4,40 +4,43 @@ import java.sql.Connection;
 import java.util.Scanner;
 
 public class Inicio {
+
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
         int opcion = 0;
-        
-        do { 
+
+        do {
             System.out.println("Selecciona una opcion:\n"
                     + "1. Crear mensaje\n"
                     + "2. Listar mensaje\n"
                     + "3. Editar mensaje\n"
                     + "4. Eliminar mensaje\n"
-                    + "5. Salir\n");  
+                    + "5. Salir\n");
             opcion = sc.nextInt();
-            
-        } while(opcion != 5);
-        
-        switch(opcion) {
-            case 1:
-                MensajeService.crearMensaje();
-                break;
-            case 2:
-                MensajeService.listarMensajes();
-                break;
-            case 3:
-                MensajeService.editarMensaje();
-                break;
-            case 4:
-                MensajeService.borrarMensaje();
-                break;
-            default:
-                System.out.println("Seleccione una opción correcta");
-                break;
-        }
-        
-        
+
+            switch (opcion) {
+                case 1:
+                    MensajeService.crearMensaje();
+                    break;
+                case 2:
+                    MensajeService.listarMensajes();
+                    break;
+                case 3:
+                    MensajeService.editarMensaje();
+                    break;
+                case 4:
+                    MensajeService.borrarMensaje();
+                    break;
+                case 5:
+                    System.out.println("\nSaliste\n");
+                    break;
+                default:
+                    System.out.println("Seleccione una opción correcta");
+                    break;
+            }
+
+        } while (opcion != 5);
+
     }
 }
