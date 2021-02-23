1baa3f68f47517c5331cb5911f7cde7a50630282
DESKTOP-QDBK0G5\Alma <ti0911291m@gmail.com>
2021-02-23 11:51:37 -0600

    Revert "Cuarto commit"
    This reverts commit c0013ca3e2d902be2bead4b094d68ffbb151b04f.

--- src/main/java/com/platzi/mensajes_app/MensajeDAO.java
+++ src/main/java/com/platzi/mensajes_app/MensajeDAO.java
@@ -1,122 +1,20 @@
 package com.platzi.mensajes_app;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 public class MensajeDAO {
-
-    public static void crearMensaje(Mensaje mensaje) {
-
-        Conexion dbConnect = new Conexion();
-        PreparedStatement ps = null;
-
-        try (Connection conexion = dbConnect.getConexion()) {
-
-            try {
-
-                String query = "INSERT INTO mensajes(mensaje, autor_mensaje) VALUES(?,?)";
-                ps = conexion.prepareStatement(query);
-                ps.setString(1, mensaje.getMensaje());
-                ps.setString(2, mensaje.getAutorMensaje());
-                ps.executeUpdate();
-                System.out.println("Mensaje creado\n");
-
-            } catch (SQLException ex) {
-                System.out.println("No se puede registrar el mensaje en la BD: ");
-                ex.printStackTrace(System.out);
-            }
-
-        } catch (SQLException ex) {
-            ex.printStackTrace(System.out);
-        }
-
+    
+    public static void crearMensaje(Mensaje mensaje){
+        
     }
-
-    public static void leerMensajes() {
-
-        Conexion dbConnect = new Conexion();
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        try (Connection conexion = dbConnect.getConexion()) {
-
-            String query = "SELECT * FROM mensajes";
-            ps = conexion.prepareStatement(query);
-            rs = ps.executeQuery();
-
-            System.out.println("Lista de mensajes:");
-
-            while (rs.next()) {
-                System.out.print("\t- ");
-                System.out.print(rs.getInt("id_mensaje") + " ");
-                System.out.print(rs.getString("mensaje") + " ");
-                System.out.print(rs.getString("autor_mensaje") + " ");
-                System.out.println(rs.getString("fecha_mensaje"));
-            }
-
-            System.out.println("");
-
-        } catch (SQLException ex) {
-            System.out.println("No se pudierón recuperar los mensajes: ");
-            ex.printStackTrace(System.out);
-        }
-
+    
+    public static void leerMnesajes() {
+        
+    }
+    
+    public static void borrarMensaje(int idMensaje) {
+    
     }
     
     public static void actualizarMensaje(Mensaje mensaje) {
-        
-        Conexion dbConnect = new Conexion();
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        
-        try (Connection conexion = dbConnect.getConexion()) {
-            
-            try {
-                
-                String query = "UPDATE mensajes SET mensaje = ? WHERE id_mensaje = ?";
-                ps = conexion.prepareStatement(query);
-                ps.setString(1, mensaje.getMensaje());
-                ps.setInt(2, mensaje.getIdMensaje());
-                ps.executeUpdate();
-                System.out.println("Mensaje actualizado\n");
-                
-            } catch (SQLException ex) { 
-                ex.printStackTrace(System.out);
-            }
-            
-        } catch (SQLException ex) {
-            ex.printStackTrace(System.out);
-        }
-        
+    
     }
-
-    public static void borrarMensaje(int idMensaje) {
-
-        Conexion dbConnect = new Conexion();
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        try (Connection conexion = dbConnect.getConexion()) {
-
-            try {
-                
-                String query = "DELETE FROM mensajes WHERE id_mensaje = ?";
-                ps = conexion.prepareStatement(query);
-                ps.setInt(1, idMensaje);
-                ps.executeUpdate();
-                System.out.println("Mensaje eliminado\n");
-
-            } catch (SQLException ex) {
-                System.out.println("No se pudo eliminar el mensaje " + idMensaje + ":");
-                ex.printStackTrace(System.out);
-            }
-
-        } catch (SQLException ex) {
-            ex.printStackTrace(System.out);
-        }
-
-    }
-
 }
--- src/main/java/com/platzi/mensajes_app/MensajeService.java
+++ src/main/java/com/platzi/mensajes_app/MensajeService.java
@@ -1,48 +1,21 @@
 package com.platzi.mensajes_app;
 
-import java.util.Scanner;
-
 public class MensajeService {
     
     public static void crearMensaje() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Escribe el mensaje:");
-        String mensaje  = sc.nextLine();
-        System.out.println("Escribe tu nombre:");
-        String autor = sc.nextLine();
-        Mensaje msj = new Mensaje(); 
-        msj.setMensaje(mensaje);
-        msj.setAutorMensaje(autor);
-        
-        MensajeDAO.crearMensaje(msj);
+    
     }
     
     public static void listarMensajes() {
-        MensajeDAO.leerMensajes();
-    }
-    
-    public static void editarMensaje() {
-        
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.println("Indica el Id del mensaje a editar:");
-        int idMensaje = Integer.parseInt(sc.nextLine());
-        
-        System.out.println("Indica el nuevo mensaje:");
-        String mensaje = sc.nextLine();
- 
-        Mensaje msj = new Mensaje();
-        msj.setIdMensaje(idMensaje);
-        msj.setMensaje(mensaje);
-        MensajeDAO.actualizarMensaje(msj);
     
     }
     
     public static void borrarMensaje() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Indica el Id del mensaje a borrar:");
-        int idMensaje = sc.nextInt();
-        MensajeDAO.borrarMensaje(idMensaje);
+    
+    }
+    
+    public static void editarMensaje() {
+    
     }
     
 }
